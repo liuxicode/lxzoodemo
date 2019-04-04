39fa12fd6d5ad00a4068b211b69781beb433f88b
liuxi <751046424@qq.com>
2019-04-04 18:14:43 +0800

    zookeeper 顺序队列

--- src/main/java/com/liuxi/zoo/Demo2.java
+++ src/main/java/com/liuxi/zoo/Demo2.java
@@ -3,6 +3,8 @@ package com.liuxi.zoo;
 import org.I0Itec.zkclient.ZkClient;
 import org.apache.zookeeper.data.Stat;
 
+import java.util.List;
+
 /**
  * @Auther: liuxi
  * @Date: 2019/4/4 14:09
@@ -17,8 +19,25 @@ public class Demo2 {
        /* zkClient.delete("/myData");
         zkClient.createPersistent("/myData","123");*/
 
-        System.out.println(zkClient.getAcl("/myData").setValue(new Stat()));
+       //System.out.println(zkClient.getAcl("/myData").setValue(new Stat()));
 
+        zkClient.createPersistent("/myLockQueue1");
+
+        String data1 = zkClient.createPersistentSequential("/myLockQueue1/","123");
+
+        System.out.println(data1);
+
+        String data2 = zkClient.createPersistentSequential("/myLockQueue1/","123");
+
+        System.out.println(data2);
+
+        String data3 = zkClient.createPersistentSequential("/myLockQueue1/","123");
+
+        System.out.println(data3);
+
+        List<String> list = zkClient.getChildren("/myLockQueue1");
+
+        System.out.println(list);
     }
 
 }
